7c0dc9353a1ea78e6e4244e2e71e66f4f0c26872
Eirini Galati <eirini.galati@gmail.com>
2018-06-30 23:06:41 +0200

    implemented client

---
+++ TransChat/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TransChat
+{
+    class Client
+    {
+
+        private TcpClient _client;
+
+        private NetworkStream _stream;
+
+        public bool Connected => _client.Connected;
+
+        public IPAddress IP => ((IPEndPoint) _client.Client.RemoteEndPoint).Address; 
+
+        public string Name { get; set; }
+
+        public void Connect(int port, string password)
+        {
+            _client = new TcpClient();
+            _client.Connect(IPAddress.Parse("127.0.0.1"), port);
+
+            //not sure if this should be here, I'm basically creating a new tcp client whenever I put in a wrong password 
+            if (!Verify(password))
+            {
+                //end the connection
+                Console.WriteLine("Wrong password!");
+            }
+
+
+            byte[] name = Encoding.ASCII.GetBytes(Name);
+            _stream = _client.GetStream();
+            _stream.Write(name, 0, name.Length);
+
+
+        }
+
+        private bool Verify(string password)
+        {
+            return true;
+        }
+
+        public void Send(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message); 
+            _stream.Write(data, 0, data.Length);
+        }
+
+        public async Task Receive()
+        {
+            byte[] receivedBytes = new byte[1024];
+            int byteCount;
+
+            await Task.Run(() =>
+            {
+                while ((byteCount = _stream.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+                {
+                    Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byteCount));
+                }
+            });
+        }
+
+    }
+}
--- TransChat/Program.cs
+++ TransChat/Program.cs
@@ -6,7 +6,28 @@ namespace TransChat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Port = 48750;
+            var client = new Client();
+
+            Console.WriteLine("Welcome to TransChat! Please enter your username.");
+
+            //get username
+            string username;
+            do
+            {
+                Console.Write(Environment.NewLine + "username: ");
+                username = Console.ReadLine();
+            } 
+            while (string.IsNullOrWhiteSpace(username));
+            client.Name = username.Trim();
+
+            
+            Console.Write(Environment.NewLine);
+            
+
+            Console.Write("server password: ");
+            var password = Console.ReadLine();
+
         }
     }
 }
